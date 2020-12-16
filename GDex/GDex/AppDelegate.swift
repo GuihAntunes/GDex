b23f59a0ef012e71486e879fe4404e3935b6431a
Guilherme Antunes Ferreira <guilherme.ferreira@itsector.pt>
2020-12-16 21:45:45 +0000

    chore(project): creating project

---
+++ GDex/GDex/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GDex
+//
+//  Created by Guilherme Antunes Ferreira on 16/12/2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
